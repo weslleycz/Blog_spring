79c6170d24820d74407dc930713c74154153c750
weslleycz <weslleysouza22@gmail.com>
2023-12-29 02:48:14 -0300

    🔧 Estrutura de projeto

---
+++ backend/src/main/java/com/blog/BlogApplication.java
@@ -0,0 +1,14 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		System.out.println(12344);
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
